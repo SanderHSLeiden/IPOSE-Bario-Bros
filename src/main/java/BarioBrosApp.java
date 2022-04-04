a67f443c01255955708a1a4d6ae25721e7a98386
Sander Bish <31567466+CrazyVito11@users.noreply.github.com>
2022-04-04 14:58:17 +0200

    Added FXGL to project

---
+++ src/main/java/BarioBrosApp.java
@@ -0,0 +1,17 @@
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class BarioBrosApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
