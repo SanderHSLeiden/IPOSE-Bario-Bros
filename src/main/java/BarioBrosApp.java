fc08fefcd9706362bb822e520981bb797676677a
linamensori <102033146+linamensori@users.noreply.github.com>
2022-04-07 13:06:13 +0200

    log in scherm

--- src/main/java/BarioBrosApp.java
+++ src/main/java/BarioBrosApp.java
@@ -5,20 +5,33 @@ import com.almasb.fxgl.app.scene.GameScene;
 import com.almasb.fxgl.app.scene.Viewport;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.entity.level.Level;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.input.virtual.VirtualButton;
 
 import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.scene.input.KeyCode;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import com.almasb.fxgl.app.services.FXGLDialogService;
+
+import javax.swing.*;
+import java.awt.*;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
+
 
 public class BarioBrosApp extends GameApplication {
 
     Entity player;
     int currentLevelInt;
     Level currentLevel;
+
+    String player1_name = null;
+    String player2_name = null;
+    boolean Answer1;
+
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -27,7 +40,7 @@ public class BarioBrosApp extends GameApplication {
         settings.setTitle("Bario Bros");
         settings.setVersion("1.0");
 
-        currentLevelInt = 1;
+      currentLevelInt = 1;
     }
 
     @Override
@@ -77,6 +90,18 @@ public class BarioBrosApp extends GameApplication {
 
     @Override
     protected void onUpdate(double tpf) {
+        if (player1_name == null || player2_name == null) {
+            loginUser();
+
+            return;
+        }
+        if(Answer1 == false){
+            loginUser();
+
+        }
+
+
+
         if (player.getY() > currentLevel.getHeight()) {
             FXGL.getGameScene().getViewport().shake(6, .2);
 
@@ -95,12 +120,34 @@ public class BarioBrosApp extends GameApplication {
         });
     }
 
+    private void loginUser() {
+
+        FXGL.getDialogService().showConfirmationBox(  "Start game", answer1 -> {
+            System.out.println("You pressed yes? " + answer1);
+            Answer1 = answer1;
+
+
+
+        });
+        FXGL.getDialogService().showInputBox("speler 2 vul je naam in:", answer -> {
+            System.out.println("You typed: "+ answer);
+            player2_name = answer;
+        });
+
+        FXGL.getDialogService().showInputBox("speler 1 vul je naam in:", answer -> {
+            System.out.println("You typed: "+ answer);
+            player1_name = answer;
+        });
+
+    }
+
     private void respawnPlayer() {
         if (player != null) {
             player.removeFromWorld();
         }
 
         player = FXGL.getGameWorld().spawn("player", 50, 50);
+
 
         Viewport viewport = FXGL.getGameScene().getViewport();
         viewport.bindToEntity(player, getAppWidth() / 2.0, getAppHeight() / 2.0);
